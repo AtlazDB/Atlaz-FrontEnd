5a57abd6f35bec44132f88b8486e64b3142d28da
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2026-03-29 21:46:25 -0300

    feat (US01): adiciona tela principal

---
+++ src/views/TelaPrincipal.vue
@@ -0,0 +1,103 @@
+<template>
+  <main class="container">
+
+    
+    <section class="background"></section>
+
+    
+    <section class="conteudo">
+
+      <header class="cabecalho">
+        <img src="../assets/logo-ipem.png" class="logo" />
+      </header>
+
+      <nav class="menu">
+        <button @click="irParaAdministrador"> Administrador </button>
+        <button> Técnico </button>
+      </nav>
+
+    </section>
+
+  </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const irParaFormulario = () => {
+  router.push('/formulario-tecnico')
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+}
+
+
+.background {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: brightness(0.5);
+}
+
+
+.conteudo {
+  .conteudo {
+  position: relative;
+  z-index: 1;
+  height: 100%;
+}
+}
+
+
+.cabecalho {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  color: #003366;
+}
+
+.logo {
+  width: 60%;
+  margin-top: 100px;
+  margin-left: 110px;
+}
+
+
+.menu {
+  position: absolute;
+  bottom: 40px;
+  right: 10%;
+  transform: translateX(-50%);
+  
+  display: flex;
+  gap: 150px;
+}
+
+button {
+  padding: 15px 30px;
+  font-size: 14px;
+  border-radius: 10px;
+  border: none;
+  background: #003366;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  background: #003366;
+}
+</style>
