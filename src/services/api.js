6e75816046df6aa8ef4ff272acb11788a4c27051
LeandroHCampos <leandrohcampos@protonmail.com>
2026-06-01 00:13:21 -0300

    chore: Adiciona api do render como baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://atlaz-backend.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
